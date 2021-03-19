2eb055c3fe6eef09e642378d093fceabe1317224
pub <pub@samsung>
2021-03-19 20:03:01 +0100

    This is my second project hello_world

---
+++ Tp001/src/isetj/servlets/helloWorld.java
@@ -0,0 +1,46 @@
+package isetj.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class helloWorld
+ */
+@WebServlet("/helloWorld")
+public class helloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public helloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("<html><head><title>servlet de bienvenu</title><head>");
+		response.getWriter().println("<body><h3><font color = 'blue'>");
+		response.getWriter().println("Hello world");
+		response.getWriter().println("</font></h3></body></html>");
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
